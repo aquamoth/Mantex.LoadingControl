f58dd5fc325f3b8f74b2128f8b9748c85a4c2051
Mattias Åslund <mattias.aslund@mantex.se>
2015-03-10 17:57:08 +0100

    Mockup of Loading Control page.

---
+++ Mantex.LoadingControl/Controllers/LoadingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mantex.LoadingControl.Controllers
+{
+    public class LoadingController : Controller
+    {
+        // GET: Loading
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mantex.LoadingControl/Models/LoadingModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mantex.LoadingControl.Models
+{
+	public class LoadingModels
+	{
+		public class IndexModel
+		{
+
+		}
+	}
+}
