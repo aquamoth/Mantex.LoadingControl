904eecd3038d10932ea02ba37b9ff47b42a8723f
Mattias Åslund <mattias.aslund@mantex.se>
2015-03-27 10:10:58 +0100

    Added basic testing of Api1Controller. Added support for publishing.

---
+++ Mantex.LoadingControl.Tests/ApiControllers/Api1ControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mantex.LoadingControl;
+using Mantex.LoadingControl.ApiControllers;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Mantex.LoadingControl.ApiModels;
+
+namespace Mantex.LoadingControl.Tests.ApiControllers
+{
+	//See: http://www.asp.net/web-api/overview/testing-and-debugging/unit-testing-controllers-in-web-api
+
+	[TestClass]
+	public class Api1ControllerTests
+	{
+		readonly Api1Controller _controller;
+
+		public Api1ControllerTests()
+		{
+			_controller = new Api1Controller();
+			_controller.Request = new HttpRequestMessage();
+			_controller.Configuration = new HttpConfiguration();
+			//_controller.Configuration.Routes.MapHttpRoute(
+			//	name: "Default",
+			//	routeTemplate: "api1/{action}/{id}",
+			//	defaults: new { controller = "Api1", action = "IsAlive", id = UrlParameter.Optional }
+			//);
+		}
+
+		[TestMethod]
+		public void IsAlive()
+		{
+			bool response = _controller.IsAlive();
+			Assert.IsTrue(response);
+		}
+
+		[TestMethod]
+		public void Transactions()
+		{
+			//// Arrange
+			//_controller.Request.RequestUri = new Uri("http://localhost/api1/transactions");
+
+			//_controller.RequestContext.RouteData = new HttpRouteData(
+			//		route: new HttpRoute(),
+			//		values: new HttpRouteValueDictionary { { "action", "transactions" } 
+			//	});
+
+			// Act
+			var transaction = new Api1Models.TransactionOperation() { Id = "42", Name = "Trans1", IsDeleted = false };
+			/*var response = */_controller.Transactions(new[] { transaction });
+
+			//// Assert
+			//Assert.AreEqual("http://localhost/api/products/42", response.Headers.Location.AbsoluteUri);
+			Assert.Inconclusive();
+		}
+
+		[TestMethod]
+		public void CountAnalysisResults()
+		{
+			int count;
+			count = _controller.CountAnalysisResults(null);
+			Assert.Inconclusive();
+		}
+
+		[TestMethod]
+		public void AnalysisResults()
+		{
+			Api1Models.AnalysisResults results = _controller.AnalysisResults(null, 1);
+			Assert.Inconclusive();
+		}
+
+		[TestMethod]
+		public void LabResults()
+		{
+			var labresult = new Api1Models.LabResultOperation() { Id = "4211", TransactionId = "42", Moisture = new Api1Models.AnalysisResultValue { Value = 46, StDev = 11 }, IsDeleted = false };
+			_controller.LabResults(new[] { labresult });
+			Assert.Inconclusive();
+		}
+	}
+}
--- Mantex.LoadingControl/ApiControllers/Api1Controller.cs
+++ Mantex.LoadingControl/ApiControllers/Api1Controller.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace Mantex.LoadingControl.Api1Controllers
+namespace Mantex.LoadingControl.ApiControllers
 {
     public class Api1Controller : ApiController
     {
@@ -21,14 +21,14 @@ namespace Mantex.LoadingControl.Api1Controllers
 		{
 			//Test by POSTing:
 			//	[{ Id: 'Trans1', IsDeleted: 'False', Supplier: 'Sunnanö', ArrivalDate: '2014-04-14T13:42:11.000+2:00' }, { Id: 'Trans2', IsDeleted: 'True' }]
-
+			
 			throw new NotImplementedException();
 			/*
 			{
 				"Message": "An error has occurred.",
 				"ExceptionMessage": "Metoden eller åtgärden har inte implementerats.",
 				"ExceptionType": "System.NotImplementedException",
-				"StackTrace": "   vid Mantex.LoadingControl.Api1Controllers.Api1Controller.Transactions(TransactionOperation[] model) i c:\\Users\\maas\\Workspace\\Git\\Mantex.LoadingControl\\Mantex.LoadingControl\\ApiControllers\\Api1Controller.cs:rad 38\r\n   vid lambda_method(Closure , Object , Object[] )\r\n   vid System.Web.Http.Controllers.ReflectedHttpActionDescriptor.ActionExecutor.<>c__DisplayClassc.<GetExecutor>b__6(Object instance, Object[] methodParameters)\r\n   vid System.Web.Http.Controllers.ReflectedHttpActionDescriptor.ActionExecutor.Execute(Object instance, Object[] arguments)\r\n   vid System.Web.Http.Controllers.ReflectedHttpActionDescriptor.ExecuteAsync(HttpControllerContext controllerContext, IDictionary`2 arguments, CancellationToken cancellationToken)\r\n--- Slut på stackspårningen från föregående plats där ett undantag utlöstes ---\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   vid System.Web.Http.Controllers.ApiControllerActionInvoker.<InvokeActionAsyncCore>d__0.MoveNext()\r\n--- Slut på stackspårningen från föregående plats där ett undantag utlöstes ---\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   vid System.Web.Http.Controllers.ActionFilterResult.<ExecuteAsync>d__2.MoveNext()\r\n--- Slut på stackspårningen från föregående plats där ett undantag utlöstes ---\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   vid System.Web.Http.Dispatcher.HttpControllerDispatcher.<SendAsync>d__1.MoveNext()"
+				"StackTrace": "   vid Mantex.LoadingControl.ApiControllers.Api1Controller.Transactions(TransactionOperation[] model) i c:\\Users\\maas\\Workspace\\Git\\Mantex.LoadingControl\\Mantex.LoadingControl\\ApiControllers\\Api1Controller.cs:rad 38\r\n   vid lambda_method(Closure , Object , Object[] )\r\n   vid System.Web.Http.Controllers.ReflectedHttpActionDescriptor.ActionExecutor.<>c__DisplayClassc.<GetExecutor>b__6(Object instance, Object[] methodParameters)\r\n   vid System.Web.Http.Controllers.ReflectedHttpActionDescriptor.ActionExecutor.Execute(Object instance, Object[] arguments)\r\n   vid System.Web.Http.Controllers.ReflectedHttpActionDescriptor.ExecuteAsync(HttpControllerContext controllerContext, IDictionary`2 arguments, CancellationToken cancellationToken)\r\n--- Slut på stackspårningen från föregående plats där ett undantag utlöstes ---\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   vid System.Web.Http.Controllers.ApiControllerActionInvoker.<InvokeActionAsyncCore>d__0.MoveNext()\r\n--- Slut på stackspårningen från föregående plats där ett undantag utlöstes ---\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   vid System.Web.Http.Controllers.ActionFilterResult.<ExecuteAsync>d__2.MoveNext()\r\n--- Slut på stackspårningen från föregående plats där ett undantag utlöstes ---\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   vid System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   vid System.Web.Http.Dispatcher.HttpControllerDispatcher.<SendAsync>d__1.MoveNext()"
 			}
 			*/
 		}
